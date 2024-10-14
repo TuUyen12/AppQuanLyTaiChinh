f7fa800bda5e9b747430a17c7e57d88ddf70ae1d
ktr1nh <22521537@gm.uit.edu.vn>
2024-10-14 12:27:11 +0700

    Giao diện SignIn SignUp

--- app/src/main/java/com/example/quanlytaichinh/MainActivity.java
+++ app/src/main/java/com/example/quanlytaichinh/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.quanlytaichinh;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,6 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
+        Button btn_sign_in = findViewById(R.id.btn_sign_in);
+        btn_sign_in.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, SignInActivity.class);
+                startActivity(intent);
+            }
+        });
+        Button btn_sign_up = findViewById(R.id.btn_sign_up);
+        btn_sign_up.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, SignUpActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
 }
